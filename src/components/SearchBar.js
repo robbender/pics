a0bc6c769fd733899d5283cc948ed44843cc608c
robbender <robb.webdev@gmail.com>
2021-06-09 22:17:15 -0700

    Added search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,17 @@
 import React from "react";
-
 class SearchBar extends React.Component {
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
   render() {
     return (
-      <div>
-        <form>
-          <input type="text" />
+      <div className="ui segment">
+        <form className="ui form">
+          <label>
+            <h3>Image Search</h3>
+          </label>
+          <input className="field" type="text" onChange={this.onInputChange} />
         </form>
       </div>
     );
