2f8147e34b2b32d64061e2042808c7fddfbcec07
robbender <robb.webdev@gmail.com>
2021-06-11 13:17:04 -0700

    Removed axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,27 @@
 import React from "react";
-import axios from "axios";
 import SearchBar from "./SearchBar";
+import unsplash from "../api/unsplash";
+import ImageList from "./ImageList";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get("https://api.unsplash.com/search/photos", {
+  state = { images: [] };
+
+  onSearchSubmit = async (term) => {
+    const response = await unsplash.get("/search/photos", {
       params: { query: term },
-      headers: {
-        Authorization: "Client-ID 9XvhPxp1mKzFZa1VG7uSbj1gWgdBRr1PyK7kf9-0Nz0",
-      },
     });
-  }
+    // console.log(response.data.results);
+    this.setState({ images: response.data.results });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <h1>Pics</h1>
         {/* Doesn't have to be 'onSubmit'; could be anything...when used on a unique component. */}
+        // goes up from child(SearchBar) to parent(App)
         <SearchBar onSubmit={this.onSearchSubmit} />
+        <ImageList images={this.state.images} />
       </div>
     );
   }
